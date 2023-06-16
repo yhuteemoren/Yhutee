ffd2839f53ba75943d938725d5444b87d34eebde
Yhutee Moren <utibeabasiumoren1@gmail.com>
2023-06-16 17:48:49 +0100

    my commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var typed = new Typed(".type-write", {
+  strings: ["Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  frontDelay: 100,
+  fronSpeed: 100,
+  loop: true,
+});
+
+// let age = prompt("what is your age?");
+
+// if (Number(age) < 18) {
+//   console.log("Sorry, you are too young to drive this car. Powering off");
+// } else if (Number(age) === 18) {
+//   console.log(
+//     "Congratulations on your first year of driving. Enjoy your ride!"
+//   );
+// } else if (Number(age) > 18) {
+//   console.log("Powering on. Enjoy your ride!");
+// }
